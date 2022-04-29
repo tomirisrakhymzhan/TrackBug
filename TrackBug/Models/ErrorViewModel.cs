3aff653a1d30482f7c218ab9939c58f621d03141
tomiris.rakhymzhan <tomiris.rakhymzhan@gmail.com>
2022-04-29 20:21:43 +0100

    first commit

---
+++ TrackBug/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TrackBug.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
